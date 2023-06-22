cb470477f185eb9aa3f1d54528b04cce930387ae
Skydoes10 <ricardoms1909@gmail.com>
2023-06-21 21:55:42 -0500

    validateJWT changed

--- src/middlewares/validateJWT.ts
+++ src/middlewares/validateJWT.ts
@@ -1,15 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
 import { handleError } from '../utils';
+import jwt from 'jsonwebtoken';
+import User from '../models/user';
+
+interface CustomRequest extends Request {
+	userId: string;
+}
+
+interface JWTData {
+	id: string;
+	iat: number;
+	exp: number;
+}
 
 export const validateJWT = (
-	req: Request,
+	req: CustomRequest,
 	res: Response,
 	next: NextFunction
 ) => {
 	const { access_token } = req.cookies;
+	if (!access_token) return handleError(401, 'Token is required', res);
+	try {
+		const { id } = jwt.verify(
+			access_token,
+			process.env.SECRET_KEY!
+		) as JWTData;
 
-	if (!access_token) {
-		return handleError(401, 'Token is required', res);
+		req.userId = id;
+	} catch (error: any) {
+		return handleError(401, error.message, res);
 	}
 	next();
 };
