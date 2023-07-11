1720c9e290b98167ae0c69f1a437d24dcce1b037
Skydoes10 <ricardoms1909@gmail.com>
2023-07-10 20:27:34 -0500

    path specified in dotenv config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import { DataSource } from 'typeorm';
 import User from '../models/user';
-dotenv.config();
+dotenv.config({
+	path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
 
 const connection = new DataSource({
 	type: 'postgres',
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import Server from './server';
 
-dotenv.config();
+dotenv.config({
+	path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`),
+});
 const server = new Server();
 server.start();
